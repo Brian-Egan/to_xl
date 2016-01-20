d0e55a93ccca2fb3977b63424d8530d0227d731b
Brian Egan DSC <BrianEgan@outlook.com>
2016-01-20 14:51:52 -0500

    Initial
    Convert ruby Time, Date, and DateTime instances to Excel integer-based
    Date values. Call '.to_xl' on an instance to use.

---
+++ lib/to_xl.rb
@@ -0,0 +1,40 @@
+require 'time'
+
+class Time
+
+  def to_excel
+    @seconds_in_day ||= 24 * 60 * 60
+    @excel_days_offset ||= 25569
+    @offset_gmt = (self.hour > self.gmtime.hour) ? true : false  # Excel dates are on GMT time, so we have to go back a day if this is calculated on one day in our time zone and another on GMT.
+    excel_date = (self.to_i/@seconds_in_day) + @excel_days_offset
+    excel_date -= 1 if @offset_gmt # Excel dates are on GMT time, so we have to go back a day if this is calculated on one day in our time zone and another on GMT.
+    excel_date
+  end
+  alias_method :to_xl, :to_excel
+
+end
+
+
+if defined?(Date)
+  class Date
+
+    def to_excel
+      self.to_time.to_excel
+    end
+    alias_method :to_xl, :to_excel
+
+  end
+end
+
+
+if defined?(DateTime)
+  class DateTime
+
+    def to_excel
+      self.to_time.to_excel
+    end
+    alias_method :to_xl, :to_excel
+    
+  end
+end
+
---
+++ to_xl.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'to_xl'
+  s.version     = '0.0.1'
+  s.date        = '2016-01-20'
+  s.summary     = "To Excel"
+  s.description = "A simple gem to convert Time, Date, and DateTime instances to Excel date integers."
+  s.authors     = ["Brian Egan"]
+  s.email       = 'brianegan@outlook.com'
+  s.files       = ["lib/to_xl.rb"]
+  s.license       = 'MIT'
+end
