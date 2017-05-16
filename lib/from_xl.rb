d5675b0d8bf0356292771c98b45fc1e6a3d3f1b5
Brian Egan <brian_egan@discovery.com>
2017-05-16 12:08:24 -0400

    Added "from_xl"
    Now Integers, floats, and Fixnum pulled from Excel date values can be
    converted into Ruby dates.

---
+++ lib/from_xl.rb
@@ -0,0 +1,42 @@
+require 'time' unless defined? Time 
+require 'date' unless defined? Date 
+require 'datetime' unless defined? DateTime
+
+class Float
+
+  def from_excel
+    seconds_in_day ||= 24.0 * 60.0 * 60.0
+    excel_days_offset ||= 25569.0
+    gmt_offset = (((Time.now.hour.to_f - Time.now.gmtime.hour.to_f)/24.0) * -1) + (0.5 / seconds_in_day) # Have to compensate for GMT offsets since Excel does by default, and then we add half a second to make sure it has the correct day (and not 23:59:59 of the previous day)
+    val = self
+    val += gmt_offset
+    new_date = Time.at((val - excel_days_offset) * seconds_in_day)
+  end
+  alias_method :from_xl, :from_excel
+
+end
+
+
+# if defined?(Date)
+  class Integer
+
+    def from_excel
+      self.to_f.from_excel
+    end
+    alias_method :from_xl, :from_excel
+
+  end
+# end
+
+
+# if defined?(DateTime)
+  class Fixnum
+
+    def from_excel
+      self.to_f.from_excel
+    end
+    alias_method :from_xl, :from_excel
+    
+  end
+# end
+
--- to_xl.gemspec
+++ to_xl.gemspec
@@ -1,12 +1,12 @@
 Gem::Specification.new do |s|
   s.name        = 'to_xl'
-  s.version     = '0.0.2'
-  s.date        = '2016-01-20'
+  s.version     = '2.0'
+  s.date        = '2017-05-16'
   s.summary     = "To Excel"
-  s.description = "A simple gem to convert Time, Date, and DateTime instances to Excel date integers."
+  s.description = "A simple gem to convert Time, Date, and DateTime instances to Excel date integers. And to convert Excel date integers/decimals to Ruby dates."
   s.authors     = ["Brian Egan"]
   s.email       = 'brianegan@outlook.com'
-  s.files       = ["lib/to_xl.rb"]
+  s.files       = ["lib/to_xl.rb", "lib/from_xl.rb"]
   s.homepage    =
     'https://github.com/Brian-Egan/to_xl'
   s.license       = 'MIT'
